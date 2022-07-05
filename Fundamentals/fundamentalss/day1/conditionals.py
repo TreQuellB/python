791b7af7501fc8553515e4f05cf2921c0eade1b1
TreQuellB <trebelle1@gmail.com>
2022-07-05 14:59:41 -0400

    first commit

---
+++ Fundamentals/fundamentalss/day1/conditionals.py
@@ -0,0 +1,10 @@
+# > < >= <= == != ! and or not
+
+num = 20
+
+
+if num> 15:
+    print(f"{num} is greater than 15.")
+    print("something else")
+else:num ==15:
+    print(f"{num} is exactly 15")
---
+++ Fundamentals/fundamentalss/day1/python_fundamentals.py
@@ -0,0 +1,48 @@
+"""""
+data types- JS
+String 
+Numbers Boolean
+null
+undefined
+
+Data types-python
+boolean     
+numbers(int/float)
+String
+
+Array
+Objects
+"""""
+# This is an inline comment
+# Js- let/var/const flag= true;
+
+flag = True
+flag = False
+grade= 9.45
+age = 20
+first_name ="Alex"
+last_name= 'Miller'
+# console.log(variableName)
+print(flag)
+print(grade)
+print(age)
+print(first_name)
+print(last_name)
+# str is used to to make this able to work or f
+print(last_name + " " + str(age))
+print(f"hello there my name is{first_name} {last_name}. And I am {age} years old")
+
+
+print(type(age))
+print(type(flag))
+print(type(first_name))
+
+print(int(grade),grade)
+
+grade = int(grade)
+
+print(int(grade), grade)
+
+print(len (first_name), len(last_name))
+
+print(first_name.upper())
