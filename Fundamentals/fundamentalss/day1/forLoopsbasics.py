2fcf696d510555d21c17eda7626d9d4ef88a4363
TreQuellB <trebelle1@gmail.com>
2022-07-05 18:18:19 -0400

    update

---
+++ Fundamentals/fundamentalss/day1/forLoopsbasics.py
@@ -0,0 +1,37 @@
+#1 Basic - Print all integers from 0 to 150.
+#2 Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+#3 Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+#4 Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+#5 Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+#6 Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+# for i in range(0,151 ):
+#     print(i)
+
+# for i in range(5,1005,5):
+#     print(i)
+
+# for i in range(1,101):
+#     if(i%10==0):
+#         print("Coding Dojo")
+#     elif(i%5==0):
+#         print("Coding")
+#     else:
+#         print(i)
+
+y=0
+for i in range(1,500001,2):
+    # if(i%2!=0):
+        y+=i
+        # print(i)
+print(y)
+
+
+for i in range(2018,0,-4):
+    print(i)
+
+
+lowNum =2
+highNum =22
+mult =4
+for i in range(lowNum,highNum,mult):
+    print(i)
