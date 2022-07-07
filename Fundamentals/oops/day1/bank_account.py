fe738c8ca3d4ecef284f313720dda83820e56e86
TreQuellB <trebelle1@gmail.com>
2022-07-07 00:53:15 -0400

    update

---
+++ Fundamentals/oops/day1/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+        # your code here! (remember, instance attributes go here)
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+        self.deposit = self.deposit + amount
+        if deposit_balance < 0:
+            print( f"Insufficient funds: Charging a $5 fee")
+        else :
+            print(self.deposit_balance)
+        # your code here
+    def withdraw(self, amount):
+        self.withdraw = self.withdraw + amount
+        # your code here
+    def display_account_info(self):
+        print(f"Balance:{self.balance}")
+        # your code here
+    def yield_interest(self):
+        self.yield_interest = 
+        # your code here
+
+        mybank_account = BankAccount()  
+
+        mybank_account = BankAccount()  
