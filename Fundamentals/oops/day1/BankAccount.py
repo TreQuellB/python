0c0117a126f0657bdccd05ad6d46af839a9d2168
TreQuellB <trebelle1@gmail.com>
2022-07-07 00:53:25 -0400

    update

--- Fundamentals/oops/day1/BankAccount.py
+++ Fundamentals/oops/day1/BankAccount.py
@@ -1,23 +1,34 @@
 class BankAccount:
-    # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance): 
-        self.balance = balance
-        self.int_rate = int_rate
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
-    def deposit(self, amount):
-        self.nomoney_balance=balance
-        if nomoney_balance < 0:
-            print( f"Insufficient funds: Charging a $5 fee")
-        else :
-            print(self.nomoney_balance)
-        # your code here
-    def withdraw(self, amount):
-        
-        # your code here
-    def display_account_info(self):
-        # your code here
-    def yield_interest(self):
-        # your code here
 
-BankAccount()
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+        
+        
+    def deposit(self, amount):
+        self.balance += amount
+        print(self.balance)
+        return self
+    def withdraw(self, amount):
+        if self.balance < amount:
+            self.balance -= 5
+            print( f"Insufficient funds: Charging a $5 fee")
+            return self
+        self.balance -= amount
+        print(self.balance)
+        return self
+        
+    def display_account_info(self):
+        print(f"Balance:${self.balance}")
+        return self
+    def yield_interest(self):
+        self.balance += self.balance * self.int_rate
+        
+        return self
+
+mybank_account = BankAccount(5,100)  
+
+mybank_account.deposit(100).deposit(10000).deposit(2).withdraw(300).withdraw(400).deposit(1).display_account_info().yield_interest().display_account_info()
+
+mybank_account2 = BankAccount(2,100)  
+mybank_account2.deposit(50).deposit(0).withdraw(25).withdraw(1).withdraw(2).withdraw(1).display_account_info()
